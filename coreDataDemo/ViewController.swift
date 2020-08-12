cee2835b79fa27ccd5b9f9416a99ea7f762448ea
Jose Sanchez <josersanchez0117@gmail.com>
2020-08-12 13:22:36 -0500

    Initial Commit

---
+++ coreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coreDataDemo
+//
+//  Created by jose sanchez on 8/12/20.
+//  Copyright © 2020 jose sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

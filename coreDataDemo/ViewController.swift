4efb7bfa21521b8b29e66ad95b68518e36af9ee6
Jose Sanchez <josersanchez0117@gmail.com>
2020-08-12 14:01:52 -0500

    Finished, hopefully

--- coreDataDemo/ViewController.swift
+++ coreDataDemo/ViewController.swift
@@ -7,12 +7,65 @@
 //
 
 import UIKit
+import Foundation
+import CoreData
 
 class ViewController: UIViewController {
-
+    var dataManager: NSManagedObjectContext!
+    var listArray = [NSManagedObject]()
+    
+    @IBOutlet var dataTextBox: UITextField!
+    @IBOutlet var dataLabel: UILabel!
+    @IBAction func onWriteDataButtonPressed(_ sender: Any) {
+        let newEntity = NSEntityDescription.insertNewObject(forEntityName: "Item", into: dataManager)
+        newEntity.setValue(dataTextBox.text!, forKey: "about")
+        do {
+            try self.dataManager.save()
+            listArray.append(newEntity)
+        } catch {
+            print("Could not save data")
+        }
+        dataLabel.text?.removeAll()
+        dataTextBox.text?.removeAll()
+        fetchData()
+    }
+    @IBAction func onDeleteDataButtonPressed(_ sender: Any) {
+        let itemToDelete = dataLabel.text!
+        for item in listArray {
+            if item.value(forKey: "about") as! String == itemToDelete {
+                dataManager.delete (item)
+            }
+            do {
+                try self.dataManager.save()
+                
+            } catch {
+                print("Error Deleting Data")
+            }
+        }
+        dataLabel.text?.removeAll()
+        dataTextBox.text?.removeAll()
+        fetchData()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        dataManager = appDelegate.persistentContainer.viewContext
+        dataLabel.text?.removeAll()
+        fetchData()
         // Do any additional setup after loading the view.
+    }
+    func fetchData() {
+        let fetchReq : NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Item")
+        do {
+            let result = try dataManager.fetch(fetchReq)
+            listArray = result as! [NSManagedObject]
+            for item in listArray {
+                let product = item.value(forKey: "about") as! String
+                dataLabel.text! = product
+            }
+        } catch {
+            print("Error!")
+        }
     }
 
 
